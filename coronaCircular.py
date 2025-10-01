900c904e841b7c20d3def2c51e25c67435596d82
Maya <173285566+MayaCS30@users.noreply.github.com>
2025-10-01 14:53:45 -0500

    sistema corona

---
+++ coronaCircular.py
@@ -0,0 +1,8 @@
+print("Corona Circular")
+radio_ma = float(input("Ingrese radio mayor: "))
+radio_ma = float(input("Ingrese radio menor: "))
+
+area = 3.1416*(radio_ma**2  - radio_me**2)
+
+print("RESULTADO")
+print("Área: ",area)          
